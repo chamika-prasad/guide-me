21ef0b1ff98fe4d845743b827590df2136aa49f2
chamika-prasad <chamikap40@gmail.com>
2023-03-04 11:11:28 +0530

    guide-me

---
+++ src/components/homepagecard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './homepagecard.css'
+
+const HomePageCard = (props) => {
+  return (
+
+          <div className="card mt-4 text-center item" style={{backgroundColor:"#f3eaea",height:"250px",width:"370px"}}>
+            <div className="card-body">
+              <h5 className="card-title">
+              {props.item.icon}
+              </h5>
+              <p className="card-text pt-4 topic">
+                {props.item.title}
+              </p>
+              <p className="card-text pt-4">
+                <small className="text-muted"><a href="#">{props.item.detail}</a></small>
+              </p>
+            </div>
+          </div>
+  )
+}
+
+export default HomePageCard
